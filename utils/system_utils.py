22a50ab01c3cebe7dab5c04c096693dbcb05a59d
mofaoss <mofajaph@gmail.com>
2026-03-06 18:08:32 +0000

    rm cpufeature

--- utils/system_utils.py
+++ utils/system_utils.py
@@ -1,15 +1,14 @@
-try:
-    import cpufeature
-except Exception:
-    cpufeature = None
-
+import sys
+import ctypes
 
 def cpu_support_avx2(config=None):
-    if cpufeature is None:
-        support = False
-    else:
+    support = False
+
+    # 仅在 Windows 环境下调用原生 API 检测
+    if sys.platform == "win32":
         try:
-            support = bool(cpufeature.CPUFeature["AVX2"])
+            # 40 对应 Windows API 中的 PF_AVX2_INSTRUCTIONS_AVAILABLE
+            support = bool(ctypes.windll.kernel32.IsProcessorFeaturePresent(40))
         except Exception:
             support = False
 
@@ -20,3 +19,7 @@ def cpu_support_avx2(config=None):
             pass
 
     return support
+
+
+if __name__ == "__main__":
+    print("AVX2 Support:", cpu_support_avx2())
